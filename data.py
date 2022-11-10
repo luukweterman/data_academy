07c3eb86c3741afcb0478c4dcf56b9864d365f63
Steppe Rottgering <srottgering@deloitte.nl>
2022-11-10 16:05:40 +0100

    class for importing stockprices

--- data.py
+++ data.py
@@ -1,32 +1,34 @@
 import requests
 import pandas as pd
 
-#retrieving data from server
-response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=demo")
 
-# Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
-# See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-if response.status_code != 200:
-    raise ValueError("Could not retrieve data, code:", response.status_code)
+class Import_StockPrices:
+    #retrieving data from server
+    response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=demo")
 
-# The service sends JSON data, we parse that into a Python datastructure
-raw_data = response.json()
+    # Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
+    # See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+    if response.status_code != 200:
+        raise ValueError("Could not retrieve data, code:", response.status_code)
 
-## CREATING a Dataframe
-data = raw_data['Time Series (5min)']
-df = pd.DataFrame(data).T.apply(pd.to_numeric)
-print(df.head())
+    # The service sends JSON data, we parse that into a Python datastructure
+    raw_data = response.json()
 
-# Next we parse the index to create a datetimeindex
-df.index = pd.DatetimeIndex(df.index)
+    ## CREATING a Dataframe
+    data = raw_data['Time Series (5min)']
+    df = pd.DataFrame(data).T.apply(pd.to_numeric)
 
-# Let's fix the column names by chopping off the first 3 characters
-df.rename(columns=lambda s: s[3:], inplace=True)
+    # Next we parse the index to create a datetimeindex
+    df.index = pd.DatetimeIndex(df.index)
 
-print(df[['open', 'high', 'low', 'close']].plot())
+    # Let's fix the column names by chopping off the first 3 characters
+    df.rename(columns=lambda s: s[3:], inplace=True)
+    print(df.head(20))
 
-# Let's take last value of the close column for every business day
-close_per_day = df.close.resample('B').last()
+    print(df[['open', 'high', 'low', 'close']].plot())
 
-print(close_per_day.plot())
+    # Let's take last value of the close column for every business day
+    close_per_day = df.close.resample('B').last()
+
+    print(close_per_day.plot())
 
