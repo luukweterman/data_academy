6bed86bf1f1b1623bb419ef31927b1008eb68f23
Steppe Rottgering <srottgering@deloitte.nl>
2022-11-11 11:21:59 +0100

    updated data + order files

--- data.py
+++ data.py
@@ -1,34 +1,41 @@
 import requests
 import pandas as pd
 
+#retrieving data from server
+response2 = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSCO.LON&outputsize=full&apikey=demo")
+response3 = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GPV.TRV&outputsize=full&apikey=demo")
+response4 = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=DAI.DEX&outputsize=full&apikey=demo")
+response5 = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=600104.SHH&outputsize=full&apikey=demo")
 
-class Import_StockPrices:
-    #retrieving data from server
-    response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=demo")
+# The service sends JSON data, we parse that into a Python datastructure
+raw_data2 = response2.json()
+raw_data3 = response3.json()
+raw_data4 = response4.json()
+raw_data5 = response5.json()
 
-    # Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
-    # See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-    if response.status_code != 200:
-        raise ValueError("Could not retrieve data, code:", response.status_code)
+## CREATING a Dataframe
+data2 = raw_data2['Time Series (Daily)']
+data3 = raw_data3['Time Series (Daily)']
+data4 = raw_data4['Time Series (Daily)']
+data5 = raw_data5['Time Series (Daily)']
 
-    # The service sends JSON data, we parse that into a Python datastructure
-    raw_data = response.json()
+df2 = pd.DataFrame(data2).T.apply(pd.to_numeric)
+df3 = pd.DataFrame(data3).T.apply(pd.to_numeric)
+df4 = pd.DataFrame(data4).T.apply(pd.to_numeric)
+df5 = pd.DataFrame(data5).T.apply(pd.to_numeric)
 
-    ## CREATING a Dataframe
-    data = raw_data['Time Series (5min)']
-    df = pd.DataFrame(data).T.apply(pd.to_numeric)
+print(df2.head())
 
-    # Next we parse the index to create a datetimeindex
-    df.index = pd.DatetimeIndex(df.index)
+dataframe = pd.DataFrame()
+dataframe['TSCO_stock'] = df2['4. close']
+dataframe['GPV.TRV'] = df3['4. close']
+dataframe['DAI.DEX'] = df4['4. close']
+dataframe['SHH'] = df5['4. close']
 
-    # Let's fix the column names by chopping off the first 3 characters
-    df.rename(columns=lambda s: s[3:], inplace=True)
-    print(df.head(20))
+print(dataframe.info)
 
-    print(df[['open', 'high', 'low', 'close']].plot())
 
-    # Let's take last value of the close column for every business day
-    close_per_day = df.close.resample('B').last()
-
-    print(close_per_day.plot())
-
+#df2 = pd.DataFrame(data2).T.apply(pd.to_numeric)
+#df3 = pd.DataFrame(data3).T.apply(pd.to_numeric)
+#df4 = pd.DataFrame(data4).T.apply(pd.to_numeric)
+#df5 = pd.DataFrame(data5).T.apply(pd.to_numeric)
---
+++ orders.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class Orders:
+    def __init__(self, stock_name, choose_stock, investment_date):
+
+    def buy_stock(self, stock_name, choose_stock, investment_date, quantity):
+        self.choose_stock = input("Please select a stock to invest in. Fill in one of the options: TSCO_stock, GPV.TRV, DAI.DEX, SHH")
+        if self.choose_stock not in ("TSCO_stock", "GPV.TRV", "DAI.DEX", "SHH"):
+            print("Wrong option, please select TSCO_stock, GPV.TRV, DAI.DEX or SHH")
+            self.choose_stock = input("Please select a stock to invest in. Fill in one of the options: TSCO_stock, GPV.TRV, DAI.DEX, SHH")
+            print(self.stock_price)
+        else:
+            self.investment_date = input("Please choose a DAY after 2020-01-01, in the given format")
+            if self.investment_date == np.nan:
+                print("You selected a day where the stock is not available")
+        self.quantity_stock = input("How many stocks do you want to buy? Select an integer."
+
+        if self.quantity_stock not in ("TSCO_stock", "GPV.TRV", "DAI.DEX", "SHH"):
+            print("Wrong option, please select TSCO_stock, GPV.TRV, DAI.DEX or SHH")
+
+    def stock_price
+        dataframe[]
