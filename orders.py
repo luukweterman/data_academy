7f3b67fd6df4ad54b9ca5041a3d102d3a3ce2e0a
Steppe Rottgering <srottgering@deloitte.nl>
2022-11-11 16:35:59 +0100

    updated orders

--- orders.py
+++ orders.py
@@ -1,25 +1,32 @@
 import numpy as np
 
-class Orders:
-    def __init__(self, stock_name, choose_stock, investment_date):
+class Buy_stock:
+    def __init__(self):
+        self.select_stock = ""
 
-    def buy_stock(self, stock_name, choose_stock, investment_date, quantity):
-        self.choose_stock = input("Please select a stock to invest in. Fill in one of the options: TSCO_stock, GPV.TRV, DAI.DEX, SHH")
-        if self.choose_stock not in ("TSCO_stock", "GPV.TRV", "DAI.DEX", "SHH"):
+    def select_stock(self, select_stock):
+        self.select_stock = input("Please select a stock to invest in. Fill in one of the options: TSCO_stock, GPV.TRV, DAI.DEX, SHH")
+        if self.select_stock not in ("TSCO_stock", "GPV.TRV", "DAI.DEX", "SHH"):
             print("Wrong option, please select TSCO_stock, GPV.TRV, DAI.DEX or SHH")
-            self.choose_stock = input("Please select a stock to invest in. Fill in one of the options: TSCO_stock, GPV.TRV, DAI.DEX, SHH")
-            print(self.stock_price)
-        else:
-            self.investment_date = input("Please choose a DAY after 2020-01-01, in the given format")
-            if self.investment_date == np.nan:
-                print("You selected a day where the stock is not available")
-        self.quantity_stock = input("How many stocks do you want to buy? Select an integer."
+            self.select_stock = input("Please select a stock to invest in. Fill in one of the options: TSCO_stock, GPV.TRV, DAI.DEX, SHH")
+    pass
 
-        if self.quantity_stock not in ("TSCO_stock", "GPV.TRV", "DAI.DEX", "SHH"):
-            print("Wrong option, please select TSCO_stock, GPV.TRV, DAI.DEX or SHH")
+    def select_date(self, date):
+        self.date = input("Please choose a DAY after 2020-01-01, in the given format")
+        if self.date == np.nan:
+            print("You selected a day where the stock price is not available")
+            self.date = input("Please choose a DAY after 2020-01-01, in the given format")
+    pass
 
-    def stock_price
-        dataframe[]
+print(Buy_stock.select_stock("", "TSCO_stock"))
+
+
+
+#def select_quantity(self, quantity):
+ #   self.quantity_stock = input("How many stocks do you want to buy? Select an integer."
+  #  return f"You ordered {self.quantity_stock} from {self.select_stock}"
+
+
 
 class User:
     def __init__(self, name):
@@ -31,6 +38,6 @@ class Stock:
     def __init__(self, name):
         self.Stock = 0
         self.name = name
-        self.quantity =
+        self.quantity = 0
         self.price = self.value
         pass
