1547966530d5b3f63fabaf0ab5d32a16835ba814
lweterman <lweterman@deloitte.nl>
2022-11-14 13:11:12 +0100

    commit #2 branch Luuk

--- data.py
+++ data.py
@@ -5,39 +5,39 @@ import numpy as np
 #retrieving data from server
 client = requests.Session() #added this to make it faster (RJ recommended)
 response2 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSCO.LON&outputsize=full&apikey=demo")
-# response3 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GPV.TRV&outputsize=full&apikey=demo")
-# response4 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=000002.SHZ&outputsize=full&apikey=demo")
-# #response4 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=DAI.DEX&outputsize=full&apikey=demo")
+response3 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GPV.TRV&outputsize=full&apikey=demo")
+response4 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=000002.SHZ&outputsize=full&apikey=demo")
+#response4 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=DAI.DEX&outputsize=full&apikey=demo")
 # #changed the stock to 000002.SHZ since the DAI.DEX did not have values
 # response5 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=600104.SHH&outputsize=full&apikey=demo")
 
 # The service sends JSON data, we parse that into a Python datastructure
 raw_data2 = response2.json()
-# raw_data3 = response3.json()
-# raw_data4 = response4.json()
+raw_data3 = response3.json()
+raw_data4 = response4.json()
 # raw_data5 = response5.json()
 
 ## CREATING a Dataframe
 data2 = raw_data2['Time Series (Daily)']
-# data3 = raw_data3['Time Series (Daily)']
-# data4 = raw_data4['Time Series (Daily)']
+data3 = raw_data3['Time Series (Daily)']
+data4 = raw_data4['Time Series (Daily)']
 # data5 = raw_data5['Time Series (Daily)']
 
 df2 = pd.DataFrame(data2).T.apply(pd.to_numeric)
-# df3 = pd.DataFrame(data3).T.apply(pd.to_numeric)
-# df4 = pd.DataFrame(data4).T.apply(pd.to_numeric)
+df3 = pd.DataFrame(data3).T.apply(pd.to_numeric)
+df4 = pd.DataFrame(data4).T.apply(pd.to_numeric)
 # df5 = pd.DataFrame(data5).T.apply(pd.to_numeric)
-
-print(df2.head())
 
 dataframe = pd.DataFrame()
 dataframe['TSCO_stock'] = df2['4. close']
-# dataframe['GPV.TRV'] = df3['4. close']
-# dataframe['000002.SHZ'] = df4['4. close']
+dataframe['GPV.TRV'] = df3['4. close']
+dataframe['000002.SHZ'] = df4['4. close']
 # dataframe['SHH'] = df5['4. close']
 
+stock_list = list(dataframe.columns)
+print(stock_list)
+
 dataframe.info() #changed the code here
-print(dataframe.head()) #added to show overall stock closing prices
 
 #df2 = pd.DataFrame(data2).T.apply(pd.to_numeric)
 #df3 = pd.DataFrame(data3).T.apply(pd.to_numeric)
@@ -47,29 +47,74 @@ df = dataframe
 
 
 #retrieve the latest price for the TSCO stock
-def stockprice(price_series):
-    price = price_series.TSCO_stock[0]
-    return price
+def stockprice(price_series, stock_name):
+    price = price_series[stock_name]
+    last_price = price[0]
+    return last_price
 
 print(stockprice(df))
-def order_buy(dataframe):
-    #inputs
-    cash = int(input("Enter your cash balance: "))
-    quantity = int(input("Enter the amount of stocks you want to buy: "))
 
+def order(dataframe):
+    #inputs
+
+    cash = int(input("Enter your cash balance: "))
+    buy_or_sell = input("Enter your whether you want to buy [B] or sell [S]: ")
+    quantity = int(input("Enter the amount of stocks: "))
+    stock_price = stockprice(dataframe)
 
     #transaction and balance settlement
-    stock_price = stockprice(dataframe)
-    buy_price = stock_price * quantity
-    cash_balance = cash - buy_price
 
-    #condition statement for sale and cas
+    price = stock_price * quantity
+    if buy_or_sell == 'B':
+        cash_balance = cash - price
+        print(cash_balance)
+    elif buy_or_sell == 'S':
+        cash_balance = cash + price
+        print(cash_balance)
+    else:
+        print("Please select the option buy [B] or sell [S]")
+
+
+    #condition statement
     if cash_balance < 0:
-        print("Transaction failed.")
+        print("Transaction failed due to insufficient cash balance.")
         return f"This is your cash balance: {cash_balance}."
     else:
         print("Transaction not failed.")
         return f"This is your cash balance: {cash_balance}."
 
-order_buy(df)
+order(df)
+
+
+
+
+# create a user
+# class User:
+#     def __init__(self):
+#         self.cash_balance = int(input("Insert you current cash balance: "))
+#
+#     #stockprice
+#     def stockprice(self, price_series):
+#         price = price_series.TSCO_stock[0]
+#         return price
+#     #buy function
+#     def buy(self, price_series):
+#
+#         self.cash_balance -= stockprice(price_series)
+#         print(f"Cash: {self.cash_balance}")
+#
+#     def __str__(self):
+#         return f"This user has {self.cash_balance} units of cash."
+#
+#
+# #run the class
+# if __name__ == "__main__":
+#     user = User()
+#     user.buy(df)
+#     print(user)
+
+
+
+
+
 
--- orders.py
+++ orders.py
@@ -1,37 +1,109 @@
 import numpy as np
+import requests
+import pandas as pd
+
+#### Data retrieval class
+
+#retrieving data from server
+client = requests.Session() #added this to make it faster (RJ recommended)
+response2 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSCO.LON&outputsize=full&apikey=demo")
+response3 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GPV.TRV&outputsize=full&apikey=demo")
+response4 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=000002.SHZ&outputsize=full&apikey=demo")
+#response4 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=DAI.DEX&outputsize=full&apikey=demo")
+# #changed the stock to 000002.SHZ since the DAI.DEX did not have values
+# response5 = client.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=600104.SHH&outputsize=full&apikey=demo")
+
+# The service sends JSON data, we parse that into a Python datastructure
+raw_data2 = response2.json()
+raw_data3 = response3.json()
+raw_data4 = response4.json()
+# raw_data5 = response5.json()
+
+## CREATING a Dataframe
+data2 = raw_data2['Time Series (Daily)']
+data3 = raw_data3['Time Series (Daily)']
+data4 = raw_data4['Time Series (Daily)']
+# data5 = raw_data5['Time Series (Daily)']
+
+df2 = pd.DataFrame(data2).T.apply(pd.to_numeric)
+df3 = pd.DataFrame(data3).T.apply(pd.to_numeric)
+df4 = pd.DataFrame(data4).T.apply(pd.to_numeric)
+# df5 = pd.DataFrame(data5).T.apply(pd.to_numeric)
+
+dataframe = pd.DataFrame()
+dataframe['TSCO_stock'] = df2['4. close']
+dataframe['GPV.TRV'] = df3['4. close']
+dataframe['000002.SHZ'] = df4['4. close']
+# dataframe['SHH'] = df5['4. close']
+
+stock_list = list(dataframe.columns)
+print(stock_list)
+
+dataframe.info() #changed the code here
+##### Functions
+def stockprice(price_series, stock_name):
+    price = price_series[stock_name]
+    last_price = price[0]
+    return last_price
+
+def order(dataframe):
+    #inputs
+
+    cash = int(input("Enter your cash balance: "))
+    buy_or_sell = input("Enter your whether you want to buy [B] or sell [S]: ")
+    quantity = int(input("Enter the amount of stocks: "))
+    stock_price = stockprice(dataframe)
+
+    #transaction and balance settlement
+
+    price = stock_price * quantity
+    if buy_or_sell == 'B':
+        cash_balance = cash - price
+        print(cash_balance)
+    elif buy_or_sell == 'S':
+        cash_balance = cash + price
+        print(cash_balance)
+    else:
+        print("Please select the option buy [B] or sell [S]")
 
 
-class Orders:
-    def __init__(self, stock_name, choose_stock, investment_date):
+    #condition statement
+    if cash_balance < 0:
+        print("Transaction failed due to insufficient cash balance.")
+        return f"This is your cash balance: {cash_balance}."
+    else:
+        print("Transaction not failed.")
+        return f"This is your cash balance: {cash_balance}."
+#TESTING
 
-    def buy_stock(self, stock_name, choose_stock, investment_date, quantity):
-        self.choose_stock = input("Please select a stock to invest in. Fill in one of the options: TSCO_stock, GPV.TRV, DAI.DEX, SHH")
-        if self.choose_stock not in ("TSCO_stock", "GPV.TRV", "DAI.DEX", "SHH"):
-            print("Wrong option, please select TSCO_stock, GPV.TRV, DAI.DEX or SHH")
-            self.choose_stock = input("Please select a stock to invest in. Fill in one of the options: TSCO_stock, GPV.TRV, DAI.DEX, SHH")
-            print(self.stock_price)
+stockprice(dataframe, 'TSCO_stock')
+##### User input
+
+
+continue_game = "Y"
+while continue_game == "Y":
+    stock_name = input("Please select a stock to invest in. Fill in one of the options: TSCO_stock, GPV.TRV, 000002.SHZ: ").strip().upper()
+    if stock_name not in stock_list:
+        print("Wrong option, please select TSCO_stock, GPV.TRV or 000002.SHZ")
+
+        ### use stock_name as input for Luuk's retrieving of stock_price
+    else:
+        last_price = stockprice(dataframe, stock_name)
+        stock_date = input("Please choose a DAY after 2020-01-01, in the given format: ").strip().upper()
+        if stock_date == np.nan:
+            print("You selected a day where the stock price is not available")
+            ### use stock_name as input for Luuk's retrieving of stock_price
         else:
-            self.investment_date = input("Please choose a DAY after 2020-01-01, in the given format")
-            if self.investment_date == np.nan:
-                print("You selected a day where the stock is not available")
-        self.quantity_stock = input("How many stocks do you want to buy? Select an integer."
+            order(dataframe)
+            stock_quantity = int(input("Please choose the amount of stocks you want to buy. Select an integer: ").strip())
+            #stock_price = from Luuks function
+            continue_game = input("Do you like to buy more stocks? (Y/N): ")
+            if continue_game == "N":
+                    print("Game Finished")
+                    break
 
-        if self.quantity_stock not in ("TSCO_stock", "GPV.TRV", "DAI.DEX", "SHH"):
-            print("Wrong option, please select TSCO_stock, GPV.TRV, DAI.DEX or SHH")
 
-    def stock_price
-        dataframe[]
-
-class User:
-    def __init__(self, name):
-        self.name = name
-        self.sharequantity = 10
-        pass
-
-class Stock:
-    def __init__(self, name):
-        self.Stock = 0
-        self.name = name
-        self.quantity =
-        self.price = self.value
-        pass
+# player1 = Order(stock_name, stock_date, stock_quantity)
+# player2 = Order(stock_name, stock_date, stock_quantity)
+# player1.__str__()
+# player2.__str__()
